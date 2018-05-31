49609a8ec75df23bc82f6cb9e2f6375402f33140
maiya-22 <maiya_o@yahoo.com>
2018-05-31 19:58:28 -0400

    sign out via drop down in place

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,12 @@ function myFunction() {
   document.getElementById('myDropdown').classList.toggle('show');
 }
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {

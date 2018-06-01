a90a4b4633e1747fbf5b92460545e858c56f3024
maiya-22 <maiya_o@yahoo.com>
2018-05-31 23:42:28 -0400

    answers and comments to answers rendering

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tutorials
   has_many :questions
   has_many :comments
+  has_many :answers
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
